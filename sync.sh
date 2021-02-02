4414c2f7fb9579f528fd4bf4e3c76a5c3b7ff472
Chris Burgin <chris.burgin@agilebits.com>
2021-02-01 20:37:01 -0500

    sync

--- sync.sh
+++ sync.sh
@@ -3,16 +3,9 @@ cd /home/chrisburgin/backup
 
 # files to copy 
 cp -rf ~/.config/nvim .
-cp -rf ~/scripts .
 cp -f ~/.tmux.conf .
 cp -f ~/.zshrc .
 cp -f ~/.alacritty.yml .
-cp -f ~/.config/i3/config i3config
-cp -f /etc/i3status.conf .
-cp -f ~/.Xresources .
-cp -f ~/.xprofile .
-cp -f /etc/X11/xorg.conf.d/99-libinput.conf .
-cp -f /home/chrisburgin/.config/tmuxinator/b5.yml tmuxinator_b5.yml
 
 # check if there are changes
 if [[ -z $(git status -s) ]] 
