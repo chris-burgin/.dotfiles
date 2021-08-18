b6ae8d3143542b5da84e8b15160faff5b0605c17
Chris Burgin <chris.burgin@agilebits.com>
2021-08-18 14:41:07 -0400

    sync

--- nvim/lua/config-lualine.lua
+++ nvim/lua/config-lualine.lua
@@ -1,32 +1,26 @@
 local custom_papercolor_light = require'lualine.themes.papercolor_light'
 custom_papercolor_light.normal.a.bg = '#e4e4e4'
 custom_papercolor_light.normal.b.bg = '#0087af'
--- custom_papercolor_light.inactive.a.bg = '#005f87'
--- custom_papercolor_light.inactive.b.bg = '#005f87'
--- custom_papercolor_light.inactive.c.bg = '#005f87'
 custom_papercolor_light.inactive.a.bg = '#e4e4e4'
 custom_papercolor_light.inactive.b.bg = '#e4e4e4'
 custom_papercolor_light.inactive.c.bg = '#e4e4e4'
--- custom_papercolor_light.inactive.a.fg = '#efefef'
--- custom_papercolor_light.inactive.b.fg = '#efefef'
--- custom_papercolor_light.inactive.c.fg = '#efefef'
 
 require('lualine').setup({
 	options = {theme = custom_papercolor_light},
 	sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch'},
-    lualine_c = {'filename'},
+    lualine_b = {{'filename', path=1}},
+    lualine_c = {},
     lualine_x = {},
-    lualine_y = {},
+    lualine_y = {'branch'},
     lualine_z = {'location'}
   },
 	inactive_sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch'},
-    lualine_c = {'filename'},
+    lualine_b = {{'filename', path=1}},
+    lualine_c = {},
     lualine_x = {},
-    lualine_y = {},
+    lualine_y = {'branch'},
     lualine_z = {'location'}
   },
 })
