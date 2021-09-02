5de190b4c5f1163f0fad3e940fbe87afdcab7337
Chris Burgin <chris.burgin@agilebits.com>
2021-09-02 11:27:51 -0400

    sync

--- nvim/lua/config-cmp.lua
+++ nvim/lua/config-cmp.lua
@@ -1,6 +1,6 @@
 return function()
-	local lspkind = require('lspkind')
-	local cmp = require'cmp'
+  local lspkind = require('lspkind')
+  local cmp = require'cmp'
 
   cmp.setup({
     snippet = {
@@ -12,33 +12,33 @@ return function()
       ['<CR>'] = cmp.mapping.confirm({ select = true }),
     },
     sources = {
-			{ name = 'nvim_lsp' },
-			{ name = 'buffer' },
-			{ name = 'path' },
+      { name = 'nvim_lsp' },
+      { name = 'buffer' },
+      { name = 'path' },
     },
-		formatting = {
-			format = function(entry, vim_item)
-				-- fancy icons and a name of kind
-				vim_item.kind = require("lspkind").presets.default[vim_item.kind] .. " " .. vim_item.kind
+    formatting = {
+      format = function(entry, vim_item)
+        -- fancy icons and a name of kind
+        vim_item.kind = require("lspkind").presets.default[vim_item.kind] .. " " .. vim_item.kind
 
-				-- set a name for each source
-				vim_item.menu = ({
-					buffer = "[Buffer]",
-					nvim_lsp = "[LSP]",
-					luasnip = "[LuaSnip]",
-					nvim_lua = "[Lua]",
-					latex_symbols = "[Latex]",
-				})[entry.source.name]
-				return vim_item
-			end,
-		},
+        -- set a name for each source
+        vim_item.menu = ({
+          buffer = "[Buffer]",
+          nvim_lsp = "[LSP]",
+          luasnip = "[LuaSnip]",
+          nvim_lua = "[Lua]",
+          latex_symbols = "[Latex]",
+        })[entry.source.name]
+        return vim_item
+      end,
+    },
   })
 
-	local capabilities = vim.lsp.protocol.make_client_capabilities()
-	capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
-	get_bufnrs = function()
-		return vim.api.nvim_list_bufs()
-	end
+  get_bufnrs = function()
+    return vim.api.nvim_list_bufs()
+  end
 end
 
