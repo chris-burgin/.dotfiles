4822e0aa477175c8dc602f537c0c7e826692057c
Chris Burgin <chris.burgin@agilebits.com>
2021-09-13 12:05:17 -0400

    sync

--- nvim/lua/config-plugins.lua
+++ nvim/lua/config-plugins.lua
@@ -1,18 +1,13 @@
 return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
-
 	use 'tpope/vim-commentary'
 	use 'tpope/vim-fugitive'
 	use 'tpope/vim-sensible'
 	use 'tpope/vim-surround'
 	use 'tpope/vim-vinegar'
-
-
 	use {'NLKNguyen/papercolor-theme', config = require('config-theme')}
 	use {'hoob3rt/lualine.nvim', config = require('config-lualine')}
-
 	use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', config = require('config-treesitter')}
-
 	use {'neovim/nvim-lspconfig', config = require('config-lsp')}
 	use { 
 		"hrsh7th/nvim-cmp", 
@@ -24,17 +19,12 @@ return require('packer').startup(function()
 		config = require('config-cmp') 
 	}	
 	use 'onsails/lspkind-nvim'
-
 	use '~/.fzf'
 	use {'junegunn/fzf.vim', config = require('config-fzf')}
-
   use 'mhinz/vim-signify'
-
 	use {'kristijanhusak/orgmode.nvim', config = require('config-orgmode')}
-
 	use {'RRethy/vim-illuminate', config = require("config-illuminate")}
-
-	use { 'chris-burgin/query.nvim', requires = {{'nvim-lua/plenary.nvim'}}, config = require('config-query')}
+	use {'chris-burgin/query.nvim', requires = {{'nvim-lua/plenary.nvim'}}, config = require('config-query')}
 end)
 
 
