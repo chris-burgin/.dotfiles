0eda50e95c20ffd5c707633f596a881a81a6a9e3
Chris Burgin <chris.burgin@agilebits.com>
2021-12-01 20:39:19 -0500

    sync

--- nvim/lua/config-hop.lua
+++ nvim/lua/config-hop.lua
@@ -1,5 +1,6 @@
 return function()
 	require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+
 	vim.api.nvim_set_keymap('n', '<Leader>hc', "<cmd>HopChar1<cr>", {})
 	vim.api.nvim_set_keymap('n', '<Leader>hw', "<cmd>HopWord<cr>", {})
 	vim.api.nvim_set_keymap('n', '<Leader>hp', "<cmd>HopPattern<cr>", {})
