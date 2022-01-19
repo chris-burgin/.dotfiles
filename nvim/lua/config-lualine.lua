6c1491fa4f2f51e541b19b85a9d176c7c69cacf3
Chris Burgin <chris.burgin@agilebits.com>
2022-01-19 09:00:02 -0500

    sync

--- nvim/lua/config-lualine.lua
+++ nvim/lua/config-lualine.lua
@@ -18,7 +18,7 @@ return function()
 			lualine_c = {},
 			lualine_x = {},
 			lualine_y = {'branch'},
-			lualine_z = {{'diagnostics', sources={'nvim_lsp'}}, 'location'}
+			lualine_z = {{'diagnostics', sources = {'nvim_diagnostic'}}, 'location'}
 		},
 		inactive_sections = {
 			lualine_a = {'mode'},
