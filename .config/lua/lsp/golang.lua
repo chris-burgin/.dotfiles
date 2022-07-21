875f6dbf2c1f61e2d21425ceb39b1bece6be3d47
Chris Burgin <chris.burgin@agilebits.com>
2022-07-21 16:45:52 -0400

    sync

---
+++ .config/lua/lsp/golang.lua
@@ -0,0 +1,32 @@
+local util = require('lsp/_util')
+
+local function organize_imports()
+	local timeout = 1000
+	local context = { source = { organizeImports = true } }
+	vim.validate { context = { context, "t", true } }
+
+	local params = vim.lsp.util.make_range_params()
+	params.context = context
+
+	local method = "textDocument/codeAction"
+	local resp = vim.lsp.buf_request_sync(0, method, params, timeoutms)
+	if resp and resp[1] then
+		local result = resp[1].result
+		if result and result[1] then
+			local edit = result[1].edit
+			vim.lsp.util.apply_workspace_edit(edit)
+		end
+	end
+
+	vim.lsp.buf.formatting()
+end
+
+require'lspconfig'.gopls.setup({
+	on_attach = util.on_attach,
+	commands = {
+    OrganizeImports = {
+      organize_imports,
+      description = "Organize Imports"
+    }
+  }
+})
--- sync.sh
+++ sync.sh
@@ -2,14 +2,14 @@
 cd /home/chrisburgin/backup
 
 # files to copy 
-cp -f ~/.config/nvim ./.config
-cp -f ~/.config/kitty ./.config
+cp -rf ~/.config/nvim ./.config
+cp -rf ~/.config/kitty ./.config
 cp -f ~/.tmux.conf .
 cp -f ~/.zshrc .
 cp -f ~/.alacritty.yml .
 
 cp -f ~/.local/bin/muxup .
-cp -f ~/.config/muxup ./.config
+cp -rf ~/.config/muxup ./.config
 
 # check if there are changes
 if [[ -z $(git status -s) ]] 
